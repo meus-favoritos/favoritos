916a2670048150a8e7aacc38ee933592f993d9f2
lubien <lubien1996@gmail.com>
2017-02-18 15:28:45 -0300

    Fix typo

--- web/policies/session_policy.ex
+++ web/policies/session_policy.ex
@@ -3,5 +3,5 @@ defmodule App.Session.Policy do
   def can?(_user, :show, _), do: true
 
   def can?(nil, :create, _), do: true
-  def can?(_user, :crate, _), do: false
+  def can?(_user, :create, _), do: false
 end
